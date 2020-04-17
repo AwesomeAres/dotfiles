d1e48ea92b0ad1c25cc2a31944bb8a605413a6d7
AwesomeAres <Lisa.Riedel@hotmail.com>
2020-04-17 13:07:14 +0200

    make awesome awesome again

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -15,7 +15,6 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
-local net_widgets = require("net_widgets")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
